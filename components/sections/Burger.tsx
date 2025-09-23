e3685def9d58c6b5bcbc1ef795c8451ac7f1aa1e
morgan <159543660+morsig01@users.noreply.github.com>
2025-09-23 12:45:11 +0200

    Burger responsiveness

--- components/sections/Burger.tsx
+++ components/sections/Burger.tsx
@@ -1,13 +1,11 @@
-
 import React from 'react';
-
 import { AiOutlineClose } from 'react-icons/ai';
+import Link from 'next/link';
 
 type BurgerProps = {
   isOpen: boolean;
   onClose: () => void;
 };
-
 
 export const Burger = ({ isOpen, onClose }: BurgerProps) => {
   const [mounted, setMounted] = React.useState(false);
@@ -33,16 +31,16 @@ export const Burger = ({ isOpen, onClose }: BurgerProps) => {
       className={`fixed inset-0 flex items-center justify-center z-[100] transition-opacity duration-300 ${visible ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'}`}
       onTransitionEnd={handleTransitionEnd}
     >
-      <div className="w-full h-full flex flex-col relative" style={{backgroundColor: 'var(--background)' }}>
+      <div className="w-full h-full flex flex-col relative" style={{ backgroundColor: 'var(--background)' }}>
         <div className="flex flex-col items-center justify-center h-full relative">
-          <button
-            onClick={onClose}
-            className="w-10 h-10 flex items-center justify-center rounded-full cursor-pointer hover:rotate-90 transition absolute top-40 md:top-96 left-1/2 -translate-x-1/2"
-            aria-label="Close"
-          >
-            <AiOutlineClose size={30} />
-          </button>
-          <nav className="flex flex-col gap-8 text-3xl items-center justify-center h-full w-full" style={{marginTop: '4.5rem'}}>
+          <nav className="flex flex-col gap-8 text-3xl items-center justify-center h-full w-full mt-16">
+            <button
+              onClick={onClose}
+              className="mb-16 w-10 h-10 flex items-center justify-center rounded-full cursor-pointer hover:rotate-90 transition"
+              aria-label="Close"
+            >
+              <AiOutlineClose size={30} />
+            </button>
             <Link href="/" onClick={onClose} className="hover:border-b-2">/home</Link>
             <Link href="/about" onClick={onClose} className="hover:border-b-2">/about</Link>
           </nav>
